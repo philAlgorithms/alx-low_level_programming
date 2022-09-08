2d7c09f6868fb2419054b502aecac477598978d0
Philip Nwokedi <philippos764@gmail.com>
2022-09-08 04:03:27 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art"
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
