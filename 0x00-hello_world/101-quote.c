b6c176f817d6aa5829b4ac85d9add6de3cf331e9
Philip Nwokedi <philippos764@gmail.com>
2022-09-08 04:18:43 -0700

    Task 8 again 4

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - Prints "with proper grammar, but the outcome is a piece of art"
+ * main - Prints "and that piece of art is useful - Dora Korpar, 2015-10-19"
  *
  * Return: Always 0.
  */
 
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
 
 	return (1);
 }
