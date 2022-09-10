520027064442f8c00372a710de624608e93c6ad3
Philip Nwokedi <philippos764@gmail.com>
2022-09-09 21:38:55 -0700

    Task 9 Test 4

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,13 +4,11 @@
 
 /**
  * main - Prints a random number and states whether
- * it is positive, negative, or zero.
+ *        it is positive, negative, or zero.
  *
- * Return: Always 0
+ * Return: Always 0.
 */
-
 int main(void)
-
 {
 	int n;
 
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-ALX C language conditional statements
+ALX SOFWARE ENGINNERING C Language conditionals
