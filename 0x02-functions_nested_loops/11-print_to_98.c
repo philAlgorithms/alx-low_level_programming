d279a6407d4208938332a0e98b5096c417426ee1
Philip Nwokedi <philippos764@gmail.com>
2022-09-13 21:23:36 -0700

    Task 11 di nuovo 3

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - prints all natural numbers 
+ * print_to_98 - prints all natural numbers
  * from n to 98, followed by a new line.
  * @n: The number
  */
