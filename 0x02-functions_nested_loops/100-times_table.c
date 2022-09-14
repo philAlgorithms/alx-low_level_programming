69553436cfb2468617b238cbfdbe0a74c5ec3d33
Philip Nwokedi <philippos764@gmail.com>
2022-09-13 21:36:37 -0700

    mandatory 1 again 5

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the times table of the input 
+ * print_times_table - prints the times table of the input
  * starting with 0.
  * @n: The value of the times table to be printed.
  */
