b0f8f2aca85c5b0908ea231c33658692b40b0986
Philip Nwokedi <philippos764@gmail.com>
2022-09-13 18:13:53 -0700

    Task 0 Test 5

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- *
  * main - prints charachters using _putchar function
  *
  * Return: returns 0 on success
  *
  */
 
-int main() {
-	char* letters = "_putchar\n";
-	while(*letters){
+int main(void)
+{
+	char *letters = "_putchar\n";
+
+	while (*letters)
 		_putchar(*letters++);
-	}
+
 	return (0);
 }
