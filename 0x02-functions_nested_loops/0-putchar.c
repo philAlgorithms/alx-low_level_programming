68cf8e003bb245505da94531954830cd77871c0d
Philip Nwokedi <philippos764@gmail.com>
2022-09-13 17:28:56 -0700

    Task 0 test 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *
+ * main - prints charachters using _putchar function
+ *
+ * Return: returns 0 on success
+ *
+ */
+
+int main() {
+	char letters = "_putchar";
+	_putchar(letters);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ALX Nested loops
