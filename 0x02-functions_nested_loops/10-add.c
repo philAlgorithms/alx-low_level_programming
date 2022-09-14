b71d7ee3c493d79ff1139adfa8626fca07be8e87
Philip Nwokedi <philippos764@gmail.com>
2022-09-13 21:13:41 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - sums up two numbers
+ * @num1: First number
+ * @num2: Second number
+ *
+ * Return: the sum of num1 and num2
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
