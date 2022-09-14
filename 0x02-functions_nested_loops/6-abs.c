17db6c10616ddadcfc5c14a0fb7baf6620947ec0
Philip Nwokedi <philippos764@gmail.com>
2022-09-13 20:45:04 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a number
+ * @n: The number
+ *
+ * Return: the abs of the number
+ */
+int _abs(int n)
+{
+	int res = n;
+
+	if (n < 0)
+		res = -1 * n;
+
+	return (res);
+}
