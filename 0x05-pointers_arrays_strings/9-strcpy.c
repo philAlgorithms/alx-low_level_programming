fc68fb71e69ca8fcacee2e4ddc6f53c210d38e71
Philip Nwokedi <philippos764@gmail.com>
2022-09-19 21:42:04 -0700

    Task 9 again

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,9 +3,12 @@
 /**
  * _strcpy - copies the string pointed to by src,
  * including the terminating null byte (\0), to the buffer pointed to by dest
- * @str: The pointer to the string
+ * @dest: A buffer to copy the string
+ * @src: The source string to copy
+ *
+ * Return: return a  pointer to dest
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
 	int i = 0;
 
