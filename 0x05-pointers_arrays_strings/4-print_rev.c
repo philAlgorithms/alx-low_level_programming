a73df869209a9bd0782b6d591f9fecf254f88eae
Philip Nwokedi <philippos764@gmail.com>
2022-09-19 20:54:01 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * _puts - prints a string, followed by a new line, to stdout.
- * @str: The pointer to the string
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: The pointer to the string
  */
-void _puts(char *str)
+void print_rev(char *s)
 {
-	while (*str)
-		_putchar(*str++);
+	int length = 0, index = 0;
+
+	while (s[index++])
+		length++;
+
+	for (index = length - 1; index >= 0; index--)
+		_putchar(s[index]);
 
 	_putchar('\n');
 }
