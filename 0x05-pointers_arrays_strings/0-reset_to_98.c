e595879a4f4d5532921e498552bbd7d60597dd88
Philip Nwokedi <philippos764@gmail.com>
2022-09-19 19:26:08 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character.
+ * @c: The character to be checked
+ *
+ * Return: 1 if character is upercase, 0 otherwise
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
