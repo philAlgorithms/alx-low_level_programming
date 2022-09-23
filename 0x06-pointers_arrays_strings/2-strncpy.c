4f127359199e4ee28479991a71cb8233dda5e93b
Philip Nwokedi <philippos764@gmail.com>
2022-09-22 17:32:21 -0700

    Task 2 test

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies string from src to dest
+ *
+ * @dest: the string to be mutuated
+ * @src: the str that is appended to dest
+ * @n: number of bytes
+ *
+ * Return: a pointer to the resulting string dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0, dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	while (src[j] && j < n)
+	{
+		dest[dest_len++] = src[j];
+		j++;
+	}
+
+	for(j = dest_len; j < n; j++)
+		dest[j] = '\0';
+
+	return (dest);
+}
