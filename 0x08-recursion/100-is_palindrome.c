3a70bba9527857a6f7b415e7c77511d57225c0af
Philip Nwokedi <philippos764@gmail.com>
2022-09-29 05:06:03 +0100

    Task 7

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * _strlen - gets length of a string
+ * @s: pointer to string
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+		len++;
+	return (len);
+}
+/**
+ * check_palindrome - checks if string if palindome recursively
+ * @s: pointer to the string
+ * @i: current index
+ * @len: length of the string
+ *
+ * Return: 1 if palindrome or 0 if not palundrome;
+ */
+int check_palindrome(char *s, int i, int len)
+{
+	if (s[i] == s[len / 2])
+		return (1);
+
+	if (s[i] == s[len - i - 1])
+		return (check_palindrome(s, i + 1, len));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a string can be spelt the same either ways
+ * @s: a pointer to the string
+ *
+ * Return: 1 if a string is a palindrome and 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int len = _strlen(s);
+
+	if (!(*s))
+		return (1);
+
+	return (check_palindrome(s, i, len));
+}
