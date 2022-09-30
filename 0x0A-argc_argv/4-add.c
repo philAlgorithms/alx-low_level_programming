7938418e1b19bfc95daeb52b8c98070d859e22a3
Philip Nwokedi <philippos764@gmail.com>
2022-10-01 00:09:48 +0100

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds up all positive arguments
+ * @argc: number of arguments to main
+ * @argv: array of pointers to arguments to main
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
