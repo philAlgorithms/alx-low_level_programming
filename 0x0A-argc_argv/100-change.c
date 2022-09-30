9e5511a712a8ff1f3c37d30b2651c8d29d2741d4
Philip Nwokedi <philippos764@gmail.com>
2022-10-01 00:16:40 +0100

    Task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 
 /**
- * main - adds up all positive arguments
+ * main - prints the minimum number of coins
+ * to make change for an amount of money.
  * @argc: number of arguments to main
  * @argv: array of pointers to arguments to main
  *
