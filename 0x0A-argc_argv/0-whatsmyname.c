88752639b9912764a74c16359ed005b0bab52074
Philip Nwokedi <philippos764@gmail.com>
2022-09-30 23:39:23 +0100

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the function
+ * @argc: number of arguments to main
+ * @argv: array of pointers to arguments to main
+ *
+ * Return: Always 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+ALX SE - argc and argv
