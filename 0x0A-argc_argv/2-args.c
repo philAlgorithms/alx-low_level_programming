8f7cab3665322aded0537b8ea008c202012f6357
Philip Nwokedi <philippos764@gmail.com>
2022-09-30 23:47:32 +0100

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints the all arguments passed to it
+ * @argc: number of arguments to main
+ * @argv: array of pointers to arguments to main
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
