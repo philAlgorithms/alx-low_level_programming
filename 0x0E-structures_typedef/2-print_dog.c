1ddacf918883edf7f9dce53a8dbf8871b805b8f0
Philip Nwokedi <philippos764@gmail.com>
2022-10-10 20:03:53 +0100

    task 3: struct print 3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,4 +16,5 @@ void print_dog(struct dog *d)
 	d->age < 0 ? printf("Age: (nil)\n") : printf("Age: %f\n", d->age);
 
 	d->owner == NULL ? printf("Owner: (nil)\n") : printf("Owner: %s\n", d->owner);
+
 }
