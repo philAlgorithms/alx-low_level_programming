bf76cc6e66d0f10bd5e19e62490330efc6c0d35b
Philip Nwokedi <philippos764@gmail.com>
2022-10-10 18:43:29 +0100

    task 1: struct declaration 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a type describing a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog's owner
+ */
 struct dog
 {
 	char *name;
