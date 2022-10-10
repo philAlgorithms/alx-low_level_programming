49e26d847d0828f390b79e1d74393379e7c7bc55
Philip Nwokedi <philippos764@gmail.com>
2022-10-10 18:55:40 +0100

    task 2: struct initialization

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a struct of type struct
+ * @d: the declared dog struct
+ * @name: name if the dog
+ * @age: age of the dog
+ * @owner: the owner's name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
