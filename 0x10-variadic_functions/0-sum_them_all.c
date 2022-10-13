1ffd8284d7760638d1f2023f062c6414d62215c7
Philip Nwokedi <philippos764@gmail.com>
2022-10-13 17:46:04 +0100

    Task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums up a given amount of integer arguments
+ * @n: number of arguments
+ * Return: sum of arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i;
+	int sum = 0;
+
+	va_start(num, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(num, int);
+
+	va_end(num);
+	return (sum);
+}
