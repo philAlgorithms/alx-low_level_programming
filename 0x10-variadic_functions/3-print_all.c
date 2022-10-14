7bcd80b0d539437259521ecfa419e7b5ff8af8af
Philip Nwokedi <philippos764@gmail.com>
2022-10-14 01:15:30 +0100

    Task 3 test 2

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: pointer to the string
+ *
+ * Return: length of the string
+ */
+int _strlen(const char * const s)
+{
+	int res = 0;
+
+	if (*s)
+		return (res + 1 + _strlen(s + 1));
+	else
+		return (0);
+}
+
+/**
+ * print_comma - prints a comma followed by a space
+ * @i: index
+ * @len: length
+ */
+void print_comma(int i, int len)
+{
+	if (i < len - 1)
+		printf(", ");
+}
+
+/**
+ * print_all - prints anything
+ * @format - the type format of a respective parameter
+ */
+void print_all(const char * const format, ...)
+{
+	const int length = _strlen(format);
+	va_list objects;
+	int format_index = 0;
+	char *str;
+	
+	va_start(objects, format);
+	while (format_index < length)
+	{
+		switch (format[format_index]){
+		case 'f':
+			printf("%f", va_arg(objects, double));
+			print_comma(format_index, length);
+			break;
+		case 'c':
+			printf("%c", va_arg(objects, int));
+			print_comma(format_index, length);
+			break;
+		case 'i':
+			printf("%d", va_arg(objects, int));
+			print_comma(format_index, length);
+			break;
+		case 's':
+			str = va_arg(objects, char *);
+			str == NULL ? printf("(nil)") : printf("%s", str);
+			print_comma(format_index, length);
+			break;
+		}
+		
+		format_index++;
+	}
+	printf("%c", '\n');
+}
