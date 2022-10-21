cf1cda24e197a39097afd7584eb56284c64bdb77
Philip Nwokedi <philippos764@gmail.com>
2022-10-21 21:32:57 +0100

    linked list: task 0 attempt 4

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -5,7 +5,16 @@
 
 int _putchar(char c);
 
-typedef struct list_s {
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
 	char *str;
 	unsigned int len;
 	struct list_s *next;
