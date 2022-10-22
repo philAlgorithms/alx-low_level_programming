02d99561a31926fd34cc4eaaad91999dc2c294f1
Philip Nwokedi <philippos764@gmail.com>
2022-10-22 05:02:27 +0100

    task 1: get length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * list_len - gets the length of a singly linked list
+ * @h: the list struct
+ *
+ * Return: length of the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t length = 0;
+
+	for (; h != NULL; h = h->next)
+		length++;
+	return (length);
+}
