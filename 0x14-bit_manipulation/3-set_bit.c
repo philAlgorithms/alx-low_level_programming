2f39e85dbe45f51f05a7eaf0c2ba99c5435f2bee
Philip Nwokedi <philippos764@gmail.com>
2022-10-28 05:14:37 +0100

    task 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
  *
  * Return: the value of the bit at index index or -1 if an error occured
  */
-int set_bit(unsigned long int n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
